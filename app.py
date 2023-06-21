fed49a94967ebe333906fe0e97c04b785cad0a50
Manish <manish.babbar27@gmail.com>
2023-06-21 19:50:22 +0530

    initial-commit

---
+++ app.py
@@ -0,0 +1,64 @@
+import streamlit as st
+import postGen as pg
+import createCaption as cc
+
+# clear text on refresh
+def clear_text():
+    st.session_state["user_input"] = ""
+
+# Function to Write Container Styles
+def write_container_styles():
+    st.markdown(
+        """
+        <style>
+        /* CSS code for container styles */
+        .container {
+            background-color: #f2f2f2;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+            margin-bottom: 20px;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+# initialize contianer style
+write_container_styles()
+
+# Title of the App
+main = st.container()
+main.title("LinkedIn post generator from image")
+
+
+main.write('''This app a LinkedIn post from an image using the power of GPT-3.''')
+
+user_input = st.text_input('Details', placeholder=' Please provide some details for the post', key = 'user_input')
+# image_input = st.text_input('Please provide the image url', placeholder='Image url', key = 'image_input')
+
+
+
+from PIL import Image
+
+if st.button('submit'):
+    with st.spinner('Generating the post...'):
+        if user_input != "":
+            image = cc.getImage(user_input)
+            final = pg.postGen(user_input)
+            st.markdown(
+                f"""
+                <div class="container">
+                    <h2>LinkedIn Post</h2>
+                    <img src="{image}" alt="Image Description" width="200" style="display: block; margin: 0 auto;">
+                    <p>{final}</p>
+                </div>
+                """,
+                unsafe_allow_html=True
+            )
+                
+            if st.button('Refresh', on_click=clear_text):
+                pass
+
+
+
---
+++ createCaption.py
@@ -0,0 +1,30 @@
+import replicate
+
+# replicate.run(
+#   "stability-ai/stable-diffusion:27b93a2413e7f36cd83da926f3656280b2931564ff050bf9575f1fdf9bcd7478",
+#   input={"prompt": "a 19th century portrait of a wombat gentleman"}
+# )
+
+
+image = open("testImages/people.webp", "rb")
+
+def get_details(image):
+    output = replicate.run(
+            "salesforce/blip:2e1dddc8621f72155f24cf2e0adbde548458d3cab9f00c0139eea840d0ac4746",
+            input={"image": image}
+                )
+    return output
+
+def getImage(prompt):
+    import replicate
+    output = replicate.run(
+        "ai-forever/kandinsky-2:601eea49d49003e6ea75a11527209c4f510a93e2112c969d548fbb45b9c4f19f",
+        input={"prompt": prompt +  "720 p"}
+    )
+    return output[0]
+
+if __name__ == "__main__":
+    # print(get_details(image)) 
+    # output = getImage("a 19th century portrait of a wombat gentleman")
+    output = getImage("Automated museum entrance - effortless exploration.")
+    print(output)
